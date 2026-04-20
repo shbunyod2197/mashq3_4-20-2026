01ac8dcb64ed998bad32d772b3bae82a37336822
Bunyod <shbunyod2197@gmail.com>
2026-04-20 16:31:42 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1
+class User:
+    def __init__(self, username, email:)
+        self.username = username
+        self.email = email
+
+    def display(self):
+        print(f"User: {self.username}, Email: {self.email}")
+
+a= User("Bunyod", "eune2197@gmail.com")
+a.display()
+
+# 2
+class City:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def show_population(self):
+        print(f"{self.name} Aholisi: {self.population}")
+
+a = City("Xorazm", "9 mld")
+a.show_population()
